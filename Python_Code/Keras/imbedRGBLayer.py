6cbf8be97ea6fb2dad2d5a2aef370eff963548f2
Josiah Burnham <62119663+JosiahBurnham@users.noreply.github.com>
2022-11-03 10:57:09 -0400

    Add files via upload

---
+++ Python_Code/Keras/imbedRGBLayer.py
@@ -0,0 +1,47 @@
+#==========================================================
+# File   :   imbedRGB.py
+# Author :   J.Burnham
+# Date   :   02/21/2022
+# Purpose:   Imbed an greyscale image dataset into a larger rbg one
+#==========================================================
+
+import tensorflow as tf
+from tensorflow import keras
+
+
+"""Class soley for imbedding an image into a larger image, the smaller image would be surrounded by zeros.
+   And converting that image into a RGB image
+"""
+class ImbedRGB(keras.layers.Layer):
+
+    def __init__(self, desired_width, desired_height, name=None):
+        """Default Constructor for ImbedRGB
+
+        Args:
+            desired_width (int)    : the width of the image to imbed the smaller into
+            desired_height (int)   : the height of the image to imbed the smaller into
+            name (string, optional): the name of the layer to pass to Keras. Defaults to None.
+        """
+        super(ImbedRGB, self).__init__(name=name)
+        self.desired_width = desired_width
+        self.desired_height = desired_height
+
+    @tf.function
+
+    def call(self, x):
+        """_summary_
+
+        Args:
+            x (TF Tensor):  the data being pushed into the layer
+
+        Returns:
+            TF Tensor    :  the original data imbedded into an image of desired width and height
+                            and converted to rgb
+        """
+
+        x = tf.expand_dims(x, axis=3)
+        x = tf.concat([x,x,x], axis=3) # convert to rgb
+        x = tf.image.pad_to_bounding_box(x,self.desired_height//2 -20,self.desired_width//2-20,
+                                         self.desired_height,self.desired_width)
+
+        return x
